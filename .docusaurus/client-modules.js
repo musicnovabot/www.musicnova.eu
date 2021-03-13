317b99d991c98a3ea6eb7c4bf0e0a177ff9d5da0
Phyrone <phyrone@phyrone.de>
2021-03-13 21:09:07 +0100

    add gitignore and gh pages

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/samuel/Dokumente/git/wiki.musicnova.eu/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/samuel/Dokumente/git/wiki.musicnova.eu/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/samuel/Dokumente/git/wiki.musicnova.eu/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/samuel/Dokumente/git/wiki.musicnova.eu/node_modules/bootstrap/dist/css/bootstrap.min.css"),
-];
